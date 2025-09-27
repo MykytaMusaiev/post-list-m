936d42f0778cbd2fff5d9388f18033c3af9e677d
Mykyta Musaiev <n.musaiev@gmail.com>
2025-09-27 14:35:53 +0300

    feat(data): Implement JSONPlaceholder API client

---
+++ src/lib/api/jsonplaceholderService.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import { NewPostData, Post, PostDetails, RouteParamId } from '../types'
+
+const POSTS_LIMI = 20
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export const getPosts = async (): Promise<Post[]> => {
+  const response = await fetch(`${BASE_URL}/posts?_limit=${POSTS_LIMI}`, {
+    next: { revalidate: 60 },
+  })
+
+  if (!response.ok) {
+    throw new Error('Не вдалося отримати список постів.')
+  }
+
+  const data = (await response.json()) as Post[]
+  return data
+}
+
+export const getPostAndComments = async (
+  id: RouteParamId,
+): Promise<PostDetails> => {
+  const postUrl = `${BASE_URL}/posts/${id}`
+  const commentsUrl = `${BASE_URL}/posts/${id}/comments`
+
+  const [postData, commData] = await Promise.all([
+    fetch(postUrl, { next: { revalidate: 60 } }),
+    fetch(commentsUrl, { next: { revalidate: 60 } }),
+  ])
+
+  if (!postData.ok) {
+    throw new Error('Пост не знайдено.')
+  }
+
+  const [post, comments] = await Promise.all([postData.json(), commData.json()])
+
+  return { post, comments }
+}
+
+export const createPost = async (postData: NewPostData): Promise<Post> => {
+  const response = await api.post<Post>('/posts', postData)
+  return response.data
+}
